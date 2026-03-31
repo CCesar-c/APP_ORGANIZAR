65445b50e434896575c2f5a8eec4acd397ea69af
CCesar-c <cesarinfluencer14@gmail.com>
2026-03-31 15:58:28 -0300

    feat: actualiza lógica de borrado de notificaciones

--- screens.js
+++ screens.js
@@ -6,7 +6,7 @@ import {
 import {
   Platform
 } from 'react-native';
-const Notifications = Platform.OS !== 'web' ? require('expo-notifications') : null;
+const Notifications = Platform.OS !== 'web' ? require('expo-notifications'): null;
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
@@ -30,18 +30,23 @@ function Inicio() {
   const obtenerTareas = async () => {
     // await AsyncStorage.clear()
     try {
+      // console.log("cargada")
       const cargar_horas = JSON.parse(await AsyncStorage.getItem("cargar_horas"))
       const cargar_minutos = JSON.parse(await AsyncStorage.getItem("cargar_minutos"))
       horas = cargar_horas
       minutos = cargar_minutos
-      console.log(typeof (minutos))
+      // console.log(typeof (minutos))
       if (horas == undefined || horas == null && minutos == undefined || minutos == null) {
         console.log("si esta entrando en el if o esta vazio o tiene error")
-        horas = [7, 13, 19];
-        minutos = [0, 0, 0];
+        horas = [7,
+          13,
+          19];
+        minutos = [0,
+          0,
+          0];
       }
-      console.log(horas)
-      console.log(minutos)
+      //   console.log(horas)
+      // console.log(minutos)
       if (Notifications) {
         const notificaciones_agendadas = await Notifications.getAllScheduledNotificationsAsync()
         console.log(notificaciones_agendadas)
@@ -61,22 +66,37 @@ function Inicio() {
   };
 
   useEffect(() => {
-    obtenerTareas();
-  }, [2000]);
+    obtenerTareas()
+    // var id_time = setInterval(()=> {
+    //   obtenerTareas()
+    //   // alert("ejecutando el set inter")
+    // }, 5000)
+    // return (() => {
+    //   clearInterval(id_time)
+    // })()
+  }, []);
 
 
   // deletar item
   async function deleteItem(i, nome_tarea) {
     const notificaciones_agendadas = await Notifications.getAllScheduledNotificationsAsync()
-    const tarea_agendada = notificaciones_agendadas.find(n => n.content.body.includes(nome_tarea));
-
-    for (let i = 0; i < notificaciones_agendadas.length; i++) {
-      await Notifications.cancelScheduledNotificationAsync(tarea_agendada[i].identifier);
+    var borrar_agendas = []
+    // console.log(Array.isArray(borrar_agendas))
+    if (notificaciones_agendadas !== undefined) {
+      for (let i = 0; i < notificaciones_agendadas.length; i++) {
+        let notificacio_temporal = notificaciones_agendadas.find(n => n.content.body.includes(nome_tarea)) || [];
+        if (notificacio_temporal == undefined || notificacio_temporal == null) {
+          console.log("el notificacio_temporal no encontro nada")} else {
+          borrar_agendas.push(notificacio_temporal)
+          console.log(notificaciones_agendadas)
+          await Notifications.cancelScheduledNotificationAsync(borrar_agendas[i].identifier);
+        }
+      }
+      console.log("tarea borrada: ", borrar_agendas.content.body);
+      console.log("tareas restantes" + notificaciones_agendadas)
+    } else {
+      console.log("notificaciones_agendadas es undefined o tiene algo que no deveria")
     }
-
-    console.log("tarea borrada: ", tarea_agendada[0].content.body);
-    // Al crearla:
-
 
     // Al borrar la tarea:
     // await Notifications.cancelScheduledNotificationAsync(id);
@@ -86,7 +106,7 @@ function Inicio() {
 
     const lista_json = JSON.stringify(list);
     await AsyncStorage.setItem(`stacks`, lista_json);
-    console.log(lista_json);
+    // console.log(lista_json);
   }
 
   // Función para cambiar el estado de ✅ a ❌
@@ -113,63 +133,63 @@ function Inicio() {
     <View style={container}>
       <Text style={titulo}>Cosas Por Hacer</Text>
       <View
-        style={{
+        style={ {
           borderColor: "black",
           borderWidth: 1,
           width: "100%",
           flex: 1
         }}
-      >
+        >
         {
-          // console.log(datos)
-          // datos.forEach((item) => {
-          //   console.log(item.nome)
-          // })
-          datos &&
-          datos.map((item, index) => (
-            // console.log(item),
-            <View
-              key={index}
-              style={{
-                borderWidth: 1,
-                borderColor: "black",
-                margin: 5,
-                padding: 10
+        // console.log(datos)
+        // datos.forEach((item) => {
+        //   console.log(item.nome)
+        // })
+        datos &&
+        datos.map((item, index) => (
+          // console.log(item),
+          <View
+            key={index}
+            style={ {
+              borderWidth: 1,
+              borderColor: "black",
+              margin: 5,
+              padding: 10
 
-              }}
+            }}
             >
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: 10
-                }}
+            <View
+              style={ {
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 10
+              }}
               >
-                <Boton
-                  onPress={() =>
-                    alert(
-                      "descripcion: " + item.descripcion
-                    )
-                  }
+              <Boton
+                onPress={() =>
+                alert(
+                  "descripcion: " + item.descripcion
+                )
+                }
                 >
-                  {"Nome: " + item.nome}
-                </Boton>
-                <Boton
-                  onPress={() => {
-                    changeState(index);
-                  }}
+                {"Nome: " + item.nome}
+              </Boton>
+              <Boton
+                onPress={() => {
+                  changeState(index);
+                }}
                 >
-                  {`Esta echa? ${item.feita ? "✅" : "❌"}`}
-                </Boton>
-                <Boton onPress={() => deleteItem(index, item.nome)}>
-                  delete
-                </Boton>
-              </View>
-              <Text style={{ fontSize: 10 }}>{
-                "fecha de creacion: " + item.fecha}</Text>
+                {`Esta echa? ${item.feita ? "✅": "❌"}`}
+              </Boton>
+              <Boton onPress={() => deleteItem(index, item.nome)}>
+                delete
+              </Boton>
             </View>
-          ))}
+            <Text style={ { fontSize: 10 }}>{
+              "fecha de creacion: " + item.fecha}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -203,12 +223,11 @@ function Crear_tareas() {
 
   async function guardar_tarea() {
     try {
-
       // 1. Aseguramos que el canal exista (Crucial para Android)
       if (Platform.OS === 'android') {
         for (let i = 0; i < horas.length; i++) {
-          console.log(horas[i])
-          console.log(typeof (horas[i]))
+          // console.log(horas[i])
+          // console.log(typeof (horas[i]))
           var id_temporal = await Notifications.scheduleNotificationAsync({
             content: {
               title: "¡Oye! Tarea Diaria",
@@ -233,7 +252,8 @@ function Crear_tareas() {
           });
 
           const nueva_tarea = {
-            id: id_temporal, // Guardamos el ID de la notificación para poder cancelarla después
+            id: id_temporal,
+            // Guardamos el ID de la notificación para poder cancelarla después
             nome: nome,
             descripcion: descripcion,
             manana: mnn,
@@ -279,43 +299,43 @@ function Crear_tareas() {
       <Text style={titulo}>Crea una tarea</Text>
       <View
         style={[container, { gap: 10 }]}
-      >
+        >
         <TextInput
           style={inputs}
           placeholder={'nombre de la tarea'}
           onChangeText={texto => setnome(texto)}
-        />
+          />
         <TextInput
           style={inputs}
           placeholder='descripcion'
           onChangeText={texto => setdescripcion(texto)}
-        />
+          />
         {/* no se te olvide de la logica de marca si ya fue echa la tarea */}
         <Boton
           onPress={() => {
             setmnn(!mnn);
           }}
-        >{`manana?${mnn ? "✅" : "❌"}`}</Boton>
+          >{`manana?${mnn ? "✅": "❌"}`}</Boton>
 
         <Boton
           onPress={() => {
             settd(!td);
           }}
-        >{`tarde?${td ? "✅" : "❌"}`}</Boton>
+          >{`tarde?${td ? "✅": "❌"}`}</Boton>
 
         <Boton
           onPress={() => {
             setnc(!nc);
           }}
-        >{`noche?${nc ? "✅" : "❌"}`}</Boton>
+          >{`noche?${nc ? "✅": "❌"}`}</Boton>
         <Boton
           onPress={() => {
             if (Platform.OS === "android") {
-              guardar_notifications()
+              guardar_tarea();
             }
-            guardar_tarea();
+
           }}
-        >
+          >
           Crear tarea
         </Boton>
       </View>
@@ -325,11 +345,11 @@ function Crear_tareas() {
 
 function Opciones() {
   const [mnn,
-    setmnn] = useState();
+    setmnn] = useState([]);
   const [td,
-    settd] = useState();
+    settd] = useState([]);
   const [nc,
-    setnc] = useState();
+    setnc] = useState([]);
 
   async function guardar_hora() {
     if (mnn && td && nc) {
@@ -361,7 +381,7 @@ function Opciones() {
     <View style={container}>
       <Text style={titulo}>Opcoes</Text>
       <View
-        style={{
+        style={ {
           borderColor: "black",
           borderWidth: 1,
           gap: 10,
@@ -369,7 +389,7 @@ function Opciones() {
           flex: 1
         }}>
         <View
-          style={{
+          style={ {
             flexDirection: "row",
             alignItems: "center",
             gap: 10
@@ -390,15 +410,15 @@ function Opciones() {
               // console.log(array_texto_min)
               setmnn([Number(array_texto_hr.join("")), Number(array_texto_min.join(""))])
             })}
-          />
+            />
         </View>
         <View
-          style={{
+          style={ {
             flexDirection: "row",
             alignItems: "center",
             gap: 10
           }}
-        >
+          >
           <Text>coloca el horario la tarde: </Text>
           <TextInput placeholder={`ex: ${horas[1]}.${minutos[1]}`} style={inputs}
             value={td}
@@ -414,15 +434,15 @@ function Opciones() {
               // console.log(array_texto_min)
               settd([Number(array_texto_hr.join("")), Number(array_texto_min.join(""))])
             })}
-          />
+            />
         </View>
         <View
-          style={{
+          style={ {
             flexDirection: "row",
             alignItems: "center",
             gap: 10
           }}
-        >
+          >
           <Text>coloca el horario la noche: </Text>
           <TextInput placeholder={`ex: ${horas[2]}.${minutos[2]}`} style={inputs}
             value={nc}
@@ -438,7 +458,7 @@ function Opciones() {
               // console.log(array_texto_min)
               setnc([Number(array_texto_hr.join("")), Number(array_texto_min.join(""))])
             })}
-          />
+            />
         </View>
         <Boton onPress={() => {
           guardar_hora()
